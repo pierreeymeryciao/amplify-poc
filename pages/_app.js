a906759a1011b80c11117b5968e19908fd4adbbf
PEymery <pierre.eymery@ciao.ca>
2022-03-03 15:39:59 -0500

    README

--- pages/_app.js
+++ pages/_app.js
@@ -1,64 +1,68 @@
-import "../styles/globals.css";
+import '../styles/globals.css'
 
-import Amplify, { API, graphqlOperation } from "aws-amplify";
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
 
-import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-import awsExports from "../aws-exports";
-Amplify.configure(awsExports);
+import awsExports from '../aws-exports'
+Amplify.configure(awsExports)
 
-const apiName = 'todosApi';
-const path = '/todos'; 
+const apiName = 'todosApi'
+const path = '/todos'
 
-function MyApp({ Component, pageProps }) {
-  // const [formState, setFormState] = useState(initialState)
-  const [todos, setTodos] = useState([])
+function MyApp ({ Component, pageProps }){
+	// const [formState, setFormState] = useState(initialState)
+	const [ todos, setTodos ] = useState([])
 
-  useEffect(() => {
-    fetchTodos()
-  }, [])
+	useEffect(() => {
+		fetchTodos()
+	}, [])
 
-  // function setInput(key, value) {
-  //   setFormState({ ...formState, [key]: value })
-  // }
+	// function setInput(key, value) {
+	//   setFormState({ ...formState, [key]: value })
+	// }
 
-  async function fetchTodos() {
-    try {
-      API.get(apiName, path, {}).then(result => {
-        console.log(result)
-        setTodos(result.body);
-       }).catch(err => {
-        console.log(err);
-       })
-    } catch (err) { console.log('error fetching todos') }
-  }
+	async function fetchTodos (){
+		try {
+			API.get(apiName, path, {})
+				.then((result) => {
+					console.log(result)
+					setTodos(result.body)
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		} catch (err) {
+			console.log('error fetching todos')
+		}
+	}
 
-  // async function addTodo() {
-  //   try {
-  //     if (!formState.name || !formState.description) return
-  //     const todo = { ...formState }
-  //     setTodos([...todos, todo])
-  //     setFormState(initialState)
-  //     await API.graphql(graphqlOperation(createTodo, {input: todo}))
-  //   } catch (err) {
-  //     console.log('error creating todo:', err)
-  //   }
-  // }
+	// async function addTodo() {
+	//   try {
+	//     if (!formState.name || !formState.description) return
+	//     const todo = { ...formState }
+	//     setTodos([...todos, todo])
+	//     setFormState(initialState)
+	//     await API.graphql(graphqlOperation(createTodo, {input: todo}))
+	//   } catch (err) {
+	//     console.log('error creating todo:', err)
+	//   }
+	// }
 
-  return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-          <Component {...pageProps} />
-        </main>
-      )}
-    </Authenticator>
-  );
+	return (
+		<Authenticator>
+			{({ signOut, user }) => (
+				<main>
+					<h1>Hello {user.username}</h1>
+					<button onClick={signOut}>Sign out</button>
+					<Component {...pageProps} />
+				</main>
+			)}
+		</Authenticator>
+	)
 }
 
-export default MyApp;
+export default MyApp
